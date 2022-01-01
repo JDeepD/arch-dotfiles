a0a40365482800661be9a417da76a3889d5f1d94
Jaydeep Das <jaydeepjd.8914@gmail.com>
2022-01-01 22:21:42 +0530

    made changes to notinit.lua

--- .config/nvim/notinit.lua
+++ .config/nvim/notinit.lua
@@ -1,3 +1,29 @@
+-- get vars
+HOME = os.getenv("HOME")
+
+
+-- macros
+function map(mode, shortcut, command, is_noremap)
+  if (is_noremap == 0) then 
+    vim.nvim_set_keymap(mode, shortcut, command, {noremap = false, silent = true})
+  else
+    vim.nvim_set_keymap(mode, shortcut, command, {noremap = true,  silent = true})
+  end
+end
+
+function imap(shortcut, command)
+  vim.nvim_set_keymap("i", shortcut, command)
+end
+
+function nmap(shortcut, command)
+  vim.nvim_set_keymap("n", shortcut, command)
+end
+
+function tnoremap(shortcut, command)
+  vim.nvim_set_keymap("t", shortcut, command, {noremap = true})
+end
+
+
 -- Basic settings
 vim.g.mapleader = " "
 vim.o.encoding = "utf-8"
@@ -9,5 +35,20 @@ vim.o.number = "relativenumber"
 vim.o.hidden = true
 vim.o.cursorline = true
 vim.o.termguicolors = true
-vim.o.nocompatible = true
-vim.o.nocompatible = true
+
+-- Keymappings
+-- jj mapped to Esc
+imap("jj", "Esc", 0)
+
+nmap("<leader>w", "<C-w>", 1)
+tnoremap("<leader>w", "<C-\><C-n><C-w>")
+
+-- Space + i for autofixing code when possible
+nmap("<leader>i", ":CocFix<CR>", 1)
+
+-- Fast buffer switching using Ctrl + {h, l}
+nmap("<C-h>", "<C-\><C-n>:bp<CR>")
+nmap("<C-l>", "<C-\><C-n>:bn<CR>")
+
+
+
