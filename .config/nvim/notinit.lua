db3d8bc93f494fbc9a3bc521583ed9c426e945c4
Jaydeep Das <jaydeepjd.8914@gmail.com>
2022-01-01 21:41:41 +0530

    minor changes

---
+++ .config/nvim/notinit.lua
@@ -0,0 +1,13 @@
+-- Basic settings
+vim.g.mapleader = " "
+vim.o.encoding = "utf-8"
+vim.o.tabstop = 4
+vim.o.timeoutlen = 500
+vim.o.cmdheight = 2
+vim.o.updatetime = 300
+vim.o.number = "relativenumber"
+vim.o.hidden = true
+vim.o.cursorline = true
+vim.o.termguicolors = true
+vim.o.nocompatible = true
+vim.o.nocompatible = true
